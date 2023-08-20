d20d705e5a6a9b684b85a8fe85ae1be9c7e85234
jonas ribeiro <jonasribeiro41@yahoo.com.br>
2023-08-20 15:11:36 -0300

    Testando save com id nulo

--- src/test/java/com/devsuperior/dscatalog/repositories/ProductRepositoryTests.java
+++ src/test/java/com/devsuperior/dscatalog/repositories/ProductRepositoryTests.java
@@ -1,7 +1,9 @@
 package com.devsuperior.dscatalog.repositories;
 
 import com.devsuperior.dscatalog.entities.Product;
+import com.devsuperior.dscatalog.tests.Factory;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -13,6 +15,29 @@ public class ProductRepositoryTests {
 
     @Autowired
     private ProductRepository repository;
+
+    private long existingId;
+    private long nonExistingId;
+    private long countTotalProducts;
+
+    @BeforeEach
+    void SetUp() throws Exception{
+        existingId = 1L;
+        nonExistingId = 1000L;
+        countTotalProducts = 25L;
+    }
+
+    @Test
+    public void saveShouldPersistWithAutoIncrementWhenIdIsNull(){
+
+        Product product = Factory.createProduct();
+        product.setId(null);
+
+        product = repository.save(product);
+
+        Assertions.assertNotNull(product.getId());
+        Assertions.assertEquals(countTotalProducts + 1, product.getId());
+    }
 
     @Test
     public void deleteShouldDeleteObjectWhenIdExists(){
